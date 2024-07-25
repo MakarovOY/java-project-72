04ed4b1a3ea642904635628a13d6127efbfb8418
Oleg Makarov <makarovolegbox@yandex.ru>
2024-07-25 20:56:08 +0300

    update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,12 @@ plugins {
     id("java")
     jacoco
     id ("checkstyle")
+    id("io.freefair.lombok") version "8.6"
+    id("com.github.johnrengelman.shadow") version "8.1.1"
+    application
+}
+application {
+    mainClass.set("hexlet.code.App")
 }
 
 group = "hexlet.code"
@@ -12,6 +18,12 @@ repositories {
 }
 
 dependencies {
+    implementation("gg.jte:jte:3.1.9")
+    implementation("io.javalin:javalin:6.1.3")
+    implementation("io.javalin:javalin-bundle:6.1.3")
+    implementation("io.javalin:javalin-rendering:6.1.3")
+    implementation("org.slf4j:slf4j-simple:2.0.10")
+    testImplementation("org.slf4j:slf4j-simple:2.0.13")
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
---
+++ app/src/main/java/hexlet/code/App.java
@@ -0,0 +1,26 @@
+package hexlet.code;
+
+import io.javalin.Javalin;
+import io.javalin.rendering.template.JavalinJte;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class App {
+
+    public static void main(String[] args) {
+        getApp().start(getPort());
+    }
+    private static int getPort() {
+        String port = System.getenv().getOrDefault("PORT", "7070");
+        return Integer.valueOf(port);
+    }
+    public static Javalin getApp() {
+        Javalin app = Javalin.create(javalinConfig -> {
+            javalinConfig.bundledPlugins.enableDevLogging();
+            javalinConfig.fileRenderer(new JavalinJte());
+        });
+        app.get("/", ctx -> ctx.result("Hello World"));
+        return app;
+
+    }
+}
