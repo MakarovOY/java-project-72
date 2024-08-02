f65b10e70518b82957b52d3df56429048636adec
Oleg Makarov <makarovolegbox@yandex.ru>
2024-08-02 19:07:42 +0300

    update

--- app/src/main/java/hexlet/code/controllers/UrlController.java
+++ app/src/main/java/hexlet/code/controllers/UrlController.java
@@ -9,6 +9,7 @@ import io.javalin.http.Context;
 import io.javalin.http.NotFoundResponse;
 
 import java.net.URI;
+import java.net.URISyntaxException;
 import java.net.URL;
 import java.sql.SQLException;
 import java.util.List;
@@ -22,20 +23,33 @@ public class UrlController {
         String urlString = ctx.formParam("url");
         Url url = new Url(urlString);
         UrlRepository.save(url);
-        var urlTest = UrlRepository.getEntities();
-
+        try {
+            var uri = new URI(url.getName());
+            System.out.println(uri.getHost());
+            System.out.println(uri.getPath());
+            System.out.println(uri);
+        } catch (URISyntaxException e) {
+            ctx.sessionAttribute("flash", "Некорректный URL Uncorrect URL");
+            System.out.println("///////////////////блок try");
+            ctx.redirect(NamedRoots.urlsPath());
+            return;
+        }
+        ctx.sessionAttribute("flash", "Страница успешно добавлена, Page successfully added");
         ctx.redirect(NamedRoots.urlsPath());
 
 
-//        var uri = new URI(url);
+
 
     }
 
     public static void index(Context ctx) throws SQLException{
+        String flash = ctx.consumeSessionAttribute("flash");
         List<Url> urls = UrlRepository.getEntities();
         UrlsPage page = new UrlsPage(urls);
+        page.setFlash(flash);
+
         ctx.render("urls/index.jte", model("page", page));
-//        ctx.render("urls/index.jte");
+
 
     }
 
