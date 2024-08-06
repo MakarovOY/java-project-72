6b518cb6d63b7e6f79e5866826737d950e04b1a8
Oleg Makarov <makarovolegbox@yandex.ru>
2024-08-06 15:46:19 +0300

    update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,11 +47,11 @@ tasks.test {
         // showStackTraces = true
         // showCauses = true
         showStandardStreams = true
-        finalizedBy(tasks.jacocoTestReport) // report is always generated after tests run
+//        finalizedBy(tasks.jacocoTestReport) // report is always generated after tests run
     }
 }
 tasks.jacocoTestReport {
-    dependsOn(tasks.test) // tests are required to run before generating the report
+//    dependsOn(tasks.test) // tests are required to run before generating the report
     reports {
         xml.required.set(true)
     }
